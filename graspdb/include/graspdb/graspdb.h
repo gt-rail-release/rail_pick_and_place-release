7a6e0397f2b8724651ccd0df03b421ed6c83c135
Russell Toris <rctoris@wpi.edu>
2015-03-27 15:26:42 -0400

    Revert "Trimmed the branch to only the rail_pick_and_place sub directory"
    This reverts commit 4d6f96886e4e3e7e751f26cb14cd3cc88ddfbf9d.

---
+++ graspdb/include/graspdb/graspdb.h
@@ -0,0 +1,23 @@
+/*!
+ * \file graspdb.h
+ * \brief All-inclusive include file for the graspdb library.
+ *
+ * The graspdb library is a standalone C++ library for communicating with the PostgreSQL grasp database.
+ *
+ * \author Russell Toris, WPI - rctoris@wpi.edu
+ * \date March 3, 2015
+ */
+
+#ifndef RAIL_PICK_AND_PLACE_GRASPDB_GRASPDB_H_
+#define RAIL_PICK_AND_PLACE_GRASPDB_GRASPDB_H_
+
+#include "Client.h"
+#include "Entity.h"
+#include "Grasp.h"
+#include "GraspDemonstration.h"
+#include "GraspModel.h"
+#include "Orientation.h"
+#include "Pose.h"
+#include "Position.h"
+
+#endif
