b26bdd3d52d8e957ea9afca3dea52d56abcfa1c7
Russell Toris <rctoris@wpi.edu>
2015-04-06 13:57:46 -0400

    Rebase from 'upstream'

--- rail_pick_and_place_tools/include/rail_pick_and_place_tools/GraspCollectionPanel.h
+++ rail_pick_and_place_tools/include/rail_pick_and_place_tools/GraspCollectionPanel.h
@@ -1,95 +1,119 @@
 /*!
- * \GraspCollectionPanel.h
- * \brief Rviz plugin for collecting grasp data.
+ * \file GraspCollectionPanel.h
+ * \brief RViz plugin for grasp collection.
+ *
+ * The grasp collection panel allows for calling the grasp and store action server with the associated settings.
  *
  * \author David Kent, WPI - davidkent@wpi.edu
- * \date March 10, 2015
+ * \author Russell Toris, WPI - rctoris@wpi.edu
+ * \date April 6, 2015
  */
 
-#ifndef GRASP_COLLECTION_PANEL_H
-#define GRASP_COLLECTION_PANEL_H
+#ifndef RAIL_PICK_AND_PLACE_GRASP_COLLECTION_PANEL_H_
+#define RAIL_PICK_AND_PLACE_GRASP_COLLECTION_PANEL_H_
 
-#include <ros/ros.h>
+// ROS
 #include <actionlib/client/simple_action_client.h>
 #include <rail_pick_and_place_msgs/GraspAndStoreAction.h>
 #include <rviz/panel.h>
 
+// Qt
 #include <QCheckBox>
-#include <QGridLayout>
-#include <QHBoxLayout>
 #include <QLabel>
 #include <QLineEdit>
 #include <QPushButton>
-#include <QVBoxLayout>
-
-class QLineEdit;
 
 namespace rail
 {
 namespace pick_and_place
 {
 
+/*!
+ * \class GraspCollectionPanel
+ * \brief RViz plugin for grasp collection.
+ *
+ * The grasp collection panel allows for calling the grasp and store action server with the associated settings.
+ */
 class GraspCollectionPanel : public rviz::Panel
 {
-// This class uses Qt slots and is a subclass of QObject, so it needs
-// the Q_OBJECT macro.
-  Q_OBJECT
-public:
-  /**
-  * \brief Constructor
-  * @param parent parent widget
-  */
-  GraspCollectionPanel(QWidget *parent = 0);
 
-  /**
-  * \brief rviz load function, will load the max model size
-  * @param config rviz configuration
-  */
+// this class uses Qt slots and is a subclass of QObject, so it needs the Q_OBJECT macro
+Q_OBJECT
+
+public:
+  /*!
+   * \brief Create a new GraspCollectionPanel.
+   *
+   * Creates a new GraspCollectionPanel and adds the correct widgets and action client connections.
+   *
+   * \param parent The parent widget for this panel (defaults to NULL).
+   */
+  GraspCollectionPanel(QWidget *parent = NULL);
+
+  /*!
+   * \brief Load RViz configuration settings.
+   *
+   * Load the saved checkbox states if they are set.
+   *
+   * \param config The RViz configuration settings to load.
+   */
   virtual void load(const rviz::Config &config);
 
-  /**
-  * \brief rviz save function, will save the max model size
-  * @param config rviz configuration
-  */
+  /*!
+   * \brief Save RViz configuration settings.
+   *
+   * Save the checkbox states.
+   *
+   * \param config The RViz configuration settings to save.
+   */
   virtual void save(rviz::Config config) const;
 
-protected:
-  QCheckBox *lift_box_;
-  QCheckBox *verify_box_;
+private:
+  /*!
+   * \brief Callback for when the grasp and store action server finishes.
+   *
+   * Sets the status message to the corresponding message.
+   *
+   * \param state The finished goal state.
+   * \param result The result of the action client call.
+   */
+  void doneCallback(const actionlib::SimpleClientGoalState &state,
+      const rail_pick_and_place_msgs::GraspAndStoreResultConstPtr &result);
+
+  /*!
+   * \brief Callback for when the grasp and store action sends feedback.
+   *
+   * Sets the status message to the corresponding message.
+   *
+   * \param feedback The current feedback message.
+   */
+  void feedbackCallback(const rail_pick_and_place_msgs::GraspAndStoreFeedbackConstPtr &feedback);
+
+  /*! The main grasp and store action client. */
+  actionlib::SimpleActionClient<rail_pick_and_place_msgs::GraspAndStoreAction> grasp_and_store_ac_;
+
+  /*! The lift and verify grasp check boxes. */
+  QCheckBox *lift_box_, *verify_box_;
+  /*! The action server status. */
   QLabel *grasp_and_store_status_;
+  /*! The text box for the object name input. */
   QLineEdit *name_input_;
+  /*! The main grasp and store button. */
   QPushButton *grasp_and_store_button_;
 
-protected
+// used as UI callbacks
+private
   Q_SLOTS:
 
-  /**
-  * \brief call the grasp and store action server, updating the interface accordingly
-  */
+  /*!
+   * \brief Call the grasp and store action server.
+   *
+   * Calls the grasp and store action server with the given settings and disable the button.
+   */
   void executeGraspAndStore();
-
-private:
-  actionlib::SimpleActionClient <rail_pick_and_place_msgs::GraspAndStoreAction> ac_grasp_and_store_;
-
-  // The ROS node handle.
-  ros::NodeHandle nh_;
-
-  /**
-  * \brief Callback for when the grasp and store action server finishes
-  * @param state goal state
-  * @param result grasp and store result
-  */
-  void doneCb(const actionlib::SimpleClientGoalState& state, const rail_pick_and_place_msgs::GraspAndStoreResultConstPtr& result);
-
-  /**
-  * \brief Callback for feedback from the grasp and store action server
-  * @param feedback grasp and store feedback
-  */
-  void feedbackCb(const rail_pick_and_place_msgs::GraspAndStoreFeedbackConstPtr& feedback);
-
 };
 
-} // end namespace pick_and_place
-} // end namespace rail
+}
+}
 
-#endif // GRASP_COLLECTION_PANEL_H
+#endif
