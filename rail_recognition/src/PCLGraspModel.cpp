d84b905f36484e75af09f39b7ece95c816e11b17
Russell Toris <rctoris@wpi.edu>
2015-04-10 13:17:16 -0400

    Rebase from 'upstream'

--- rail_recognition/src/PCLGraspModel.cpp
+++ rail_recognition/src/PCLGraspModel.cpp
@@ -1,3 +1,15 @@
+/*!
+ * \file PCLGraspModel.cpp
+ * \brief A wrapper around a graspdb GrapsModel with a PCL point cloud.
+ *
+ * The PCLGraspModel is simply a wrapper around the graspdb GrapsModel with a PCL point cloud. A flag can also be set
+ * to mark the grasp model as an original (as apposed to newly generated during model generation). All accessors to
+ * the ROS point cloud message are disabled.
+ *
+ * \author Russell Toris, WPI - rctoris@wpi.edu
+ * \date April 8, 2015
+ */
+
 // RAIL Recognition
 #include "rail_recognition/PCLGraspModel.h"
 #include "rail_recognition/PointCloudMetrics.h"
@@ -9,6 +21,9 @@ PCLGraspModel::PCLGraspModel(const graspdb::GraspModel &grasp_model)
     : graspdb::GraspModel(grasp_model),
       pc_(new pcl::PointCloud<pcl::PointXYZRGB>)
 {
+  // default to false
+  original_ = false;
+
   // copy the point cloud if it exists
   if (grasp_model.getPointCloud().data.size() > 0)
   {
@@ -18,6 +33,16 @@ PCLGraspModel::PCLGraspModel(const graspdb::GraspModel &grasp_model)
     // simply store the header information
     pc_->header.frame_id = grasp_model.getPointCloud().header.frame_id;
   }
+}
+
+bool PCLGraspModel::isOriginal() const
+{
+  return original_;
+}
+
+void PCLGraspModel::setOriginal(const bool original)
+{
+  original_ = original;
 }
 
 const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &PCLGraspModel::getPCLPointCloud() const
